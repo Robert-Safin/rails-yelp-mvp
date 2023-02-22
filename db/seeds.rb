10773e58b05a88ec6cf10f3acd6d29d714a11d9e
Robert Safin <robertsafin98@gmail.com>
2023-02-22 14:58:11 +0800

    solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+Restaurant.create(name: "La Mère Brazier", address: "France", phone_number: "1232345", category: "french")
+Restaurant.create(name: "Banana Resto", address: "Monkey island", phone_number: "4563234", category: "belgian")
+Restaurant.create(name: "Chopstix", address: "England", phone_number: "9817516", category: "chinese")
+Restaurant.create(name: "PastaPizza", address: "Rome", phone_number: "9823647", category: "italian")
+Restaurant.create(name: "Sushi", address: "Tokyo", phone_number: "91245647", category: "japanese")
+
+puts 'Finished!'
